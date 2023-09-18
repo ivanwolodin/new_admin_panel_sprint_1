1dc4e36b178741a6492072eba93e1a60da0e7469
Ivan Volodin <ivanwolodin@gmail.com>
2023-09-18 16:50:43 +0500

    Add: fetching data by chunks

--- sqlite_to_postgres/fetcher.py
+++ sqlite_to_postgres/fetcher.py
@@ -13,73 +13,109 @@ class SQLiteExtractor:
         self._conn = connection
         self._conn.row_factory = sqlite3.Row
         self._cursor = self._conn.cursor()
+        self._chunk_size = 100
         
     def _fetch_film_works(self) -> list:
         self._cursor.execute("SELECT * FROM film_work;")
-        return [
-            FilmWork(
-                rating=movie['rating'],
-                id=movie['id'],
-                title=movie['title'],
-                description=movie['description'],
-                file_path=movie['file_path'],
-                type=movie['type'],
-                creation_date=movie['creation_date'],
-                created_at=movie['created_at'],
-                updated_at=movie['updated_at'],
-            ) 
-            for movie in self._cursor.fetchall()
-        ]
+        res = []
+        while True:
+            movies = self._cursor.fetchmany(self._chunk_size)
+            if not movies:
+                break
+            else:
+                for movie in movies:
+                    res.append(
+                        FilmWork(
+                            rating=movie['rating'],
+                            id=movie['id'],
+                            title=movie['title'],
+                            description=movie['description'],
+                            file_path=movie['file_path'],
+                            type=movie['type'],
+                            creation_date=movie['creation_date'],
+                            created_at=movie['created_at'],
+                            updated_at=movie['updated_at'],
+                        ) 
+                    )
+        return res
 
     def _fetch_persons(self) -> list:
         self._cursor.execute("SELECT * FROM person;")
-        return [
-            Person(
-                full_name=person['full_name'],
-                created_at=person['created_at'],
-                updated_at=person['updated_at'],
-                id=person['id'],
-            ) 
-            for person in self._cursor.fetchall()
-        ]
+        res = []
+        while True:
+            persons = self._cursor.fetchmany(self._chunk_size)
+            if not persons:
+                break
+            else:
+                for person in persons:
+                    res.append(
+                        Person(
+                            full_name=person['full_name'],
+                            created_at=person['created_at'],
+                            updated_at=person['updated_at'],
+                            id=person['id'],
+                        ) 
+                    )
+        return res
 
     def _fetch_genres(self) -> list:
         self._cursor.execute("SELECT * FROM genre;")
-        return [
-            Genre(
-                name=genre['name'],
-                description=genre['description'],
-                created_at=genre['created_at'],
-                updated_at=genre['updated_at'],
-                id=genre['id'],
-            ) 
-            for genre in self._cursor.fetchall()
-        ]
+        res = []
+        while True:
+            genres = self._cursor.fetchmany(self._chunk_size)
+            if not genres:
+                break
+            else:
+                for genre in genres:
+                    res.append(
+                        Genre(
+                            name=genre['name'],
+                            description=genre['description'],
+                            created_at=genre['created_at'],
+                            updated_at=genre['updated_at'],
+                            id=genre['id'],
+                        )  
+                    )
+        return res
 
     def _fetch_person_film_works(self) -> list:
         self._cursor.execute("SELECT * FROM person_film_work;")
-        return [
-            PersonFilmWork(
-                role=person_film_work['role'],
-                created_at=person_film_work['created_at'],
-                id=person_film_work['id'],
-                film_work_id=person_film_work['film_work_id'],
-                person_id=person_film_work['person_id'],
-            ) 
-            for person_film_work in self._cursor.fetchall()
-        ]
+        res = []
+        while True:
+            person_film_works = self._cursor.fetchmany(self._chunk_size)
+            if not person_film_works:
+                break
+            else:
+                for person_film_work in person_film_works:
+                    res.append(
+                        PersonFilmWork(
+                            role=person_film_work['role'],
+                            created_at=person_film_work['created_at'],
+                            id=person_film_work['id'],
+                            film_work_id=person_film_work['film_work_id'],
+                            person_id=person_film_work['person_id'],
+                        )
+                    )
+        return res
 
     def _fetch_genre_film_works(self) -> list:
         self._cursor.execute("SELECT * FROM genre_film_work;")
-        return [
-            GenreFilmWork(
-                created_at=genre_film_work['created_at'],
-                id=genre_film_work['id'],
-                film_work_id=genre_film_work['film_work_id'],
-                genre_id=genre_film_work['genre_id'],
-            ) 
-            for genre_film_work in self._cursor.fetchall()
-        ]
+        res = []
+        while True:
+            genre_film_works = self._cursor.fetchmany(self._chunk_size)
+            if not genre_film_works:
+                break
+            else:
+                for genre_film_work in genre_film_works:
+                    res.append(
+                        GenreFilmWork(
+                            created_at=genre_film_work['created_at'],
+                            id=genre_film_work['id'],
+                            film_work_id=genre_film_work['film_work_id'],
+                            genre_id=genre_film_work['genre_id'],
+                        ) 
+                    )
+        return res
 
     def extract_movies(self) -> dict:
         try:
